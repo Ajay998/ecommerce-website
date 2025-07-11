707893b7d84b19c77ba8d819f9918e889fe9bef2
Ajay <krishnaajay998@gmail.com>
2025-07-11 23:43:53 +0530

    Update Views

--- ecommerceproj/core/views.py
+++ ecommerceproj/core/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
+from django.http import JsonResponse
 
 def index(request):
     return render(request,"core/index.html")
